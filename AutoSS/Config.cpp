fb1177a57a6c7b7bd34b78b58d838d13e5703e29
lmdslyngl <lmdslyngl@users.noreply.github.com>
2017-03-14 00:30:41 +0900

    設定を表すConfigクラスを作成した

---
+++ AutoSS/Config.cpp
@@ -0,0 +1,69 @@
+﻿
+#include "Config.h"
+#include <fstream>
+
+/*
+ * 設定クラス
+*/
+
+void Config::Load(const std::string &conffile) {
+	auto mapKeyValues = ParseConfFile(conffile);
+	
+	SavePath = mapKeyValues["SavePath"];
+	WaitTime = std::stoi(mapKeyValues["WaitTime"]);
+	CaptureMethod = (CAPTURE_METHOD)std::stoi(mapKeyValues["CaptureMethod"]);
+	IncludeBorder = std::stoi(mapKeyValues["IncludeBorder"]) ? true : false;
+	HotkeyMod = std::stoi(mapKeyValues["HotkeyMod"]);
+	HotkeyCode = std::stoi(mapKeyValues["HotkeyCode"]);
+	ImageFormat = (IMAGE_FORMAT)std::stoi(mapKeyValues["ImageFormat"]);
+	
+}
+
+void Config::Save(const std::string &conffile) {
+	std::ofstream ofs(conffile);
+	if( !ofs ) {
+		throw std::exception("Failed to open conffile");
+	}
+	
+	ofs << "SavePath=" << SavePath << "\n";
+	ofs << "WaitTime=" << WaitTime << "\n";
+	ofs << "CaptureMethod=" << std::to_string(CaptureMethod) << "\n";
+	ofs << "IncludeBorder=" << (IncludeBorder ? 1 : 0) << "\n";
+	ofs << "HotkeyMod=" << std::to_string(HotkeyMod) << "\n";
+	ofs << "HotkeyCode=" << std::to_string(HotkeyCode) << "\n";
+	ofs << "ImageFormat=" << std::to_string(ImageFormat) << "\n";
+	
+	ofs.close();
+	
+}
+
+
+// 設定ファイルをキーと値に切り分ける
+std::map<std::string, std::string> Config::ParseConfFile(const std::string &conffile) {
+	std::ifstream ifs(conffile);
+	if( !ifs ) {
+		throw std::exception("Failed to open conffile");
+	}
+	
+	std::map<std::string, std::string> mapKeyValues;
+	
+	std::string line;
+	while( std::getline(ifs, line) ) {
+		// 行頭が#ならコメント
+		if( line[0] == '#' ) continue;
+		
+		std::string::size_type splitpos = line.find('=');
+		if( splitpos != std::string::npos ) {
+			std::string key = line.substr(0, splitpos);
+			std::string val = line.substr(splitpos + 1);
+			mapKeyValues[key] = val;
+		}
+		
+	}
+	
+	ifs.close();
+	
+	return mapKeyValues;
+	
+}
+
---
+++ AutoSS/Config.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include <string>
+#include <map>
+
+/*
+ * キャプチャ方法
+*/
+enum CAPTURE_METHOD {
+	CAPTURE_BITBLT,
+	CAPTURE_DESKTOP_DUPL_API
+};
+
+/*
+ * 保存形式
+*/
+enum IMAGE_FORMAT {
+	IMGFMT_PPM,
+	IMGFMT_BMP
+};
+
+/*
+ * 設定クラス
+*/
+class Config {
+public:
+	
+	Config() = default;
+	~Config() = default;
+	
+	void Load(const std::string &conffile);
+	void Save(const std::string &conffile);
+	
+	// 画像書き出しフォーマットの拡張子を取得
+	std::string GetFormatExt() const {
+		if( ImageFormat == IMGFMT_BMP ) return "bmp";
+		else if( ImageFormat == IMGFMT_PPM ) return "ppm";
+		return "";
+	}
+	
+public:
+	std::string SavePath;
+	unsigned int WaitTime;
+	CAPTURE_METHOD CaptureMethod;
+	bool IncludeBorder;
+	int HotkeyMod;
+	int HotkeyCode;
+	IMAGE_FORMAT ImageFormat;
+	
+private:
+	// 設定ファイルをキーと値に切り分ける
+	std::map<std::string, std::string> ParseConfFile(const std::string &conffile);
+	
+};
+
