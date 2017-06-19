300e584bc10ced6753c8b526daed8d3a11b587ed
lmdslyngl <lmdslyngl@users.noreply.github.com>
2017-06-19 21:42:22 +0900

    バースト撮影を行うScreenShotBurstを追加した

---
+++ AutoSS/ScreenShotBurst.cpp
@@ -0,0 +1,103 @@
+﻿
+#include "ScreenShotBurst.h"
+
+ScreenShotBurst::ScreenShotBurst(
+	std::shared_ptr<CaptureBase> &pCap,
+	std::shared_ptr<ImageWriterBase> &pWriter,
+	const std::string &savePathFormat,
+	TRIMMING_MODE trimMode,
+	unsigned int numCaptureImages)
+	: ScreenShotBase()
+{
+	
+	this->SetCapturer(pCap);
+	this->SetWriter(pWriter);
+	this->SetSavePathFormat(savePathFormat);
+	this->SetTrimmingMode(trimMode);
+	this->NumCaptureImages = numCaptureImages;
+	
+}
+
+ScreenShotBurst::~ScreenShotBurst() {
+	Stop();
+}
+
+// 連写開始
+void ScreenShotBurst::Start() {
+	TakenCount = 0;
+	StopFlag = false;
+	TakeThread = std::thread([this]() { this->TakeSSFunc(); });
+}
+
+// 連写終了
+void ScreenShotBurst::Stop() {
+	StopFlag = true;
+	if( TakeThread.joinable() ) TakeThread.join();
+}
+
+// スクリーンショット撮影
+void ScreenShotBurst::TakeSSFunc() {
+	unsigned char *bufcursor = nullptr;
+	unsigned char *bufend = nullptr;
+	int sswidth, ssheight;
+	
+	vecImgSize.reserve(NumCaptureImages);
+	
+	// 撮影
+	for( int i = 0; i < NumCaptureImages; i++ ) {
+		if( StopFlag ) break;
+		
+		HWND hCaptureWindow = GetForegroundWindow();
+		RECT windowRect = GetWindowRegion(hCaptureWindow);
+		windowRect = ClampOutOfRegion(windowRect);
+		
+		unsigned int buflen = pCap->CalcNecessaryBufferLength(&windowRect);
+		if( bufcursor == nullptr ) {
+			// まだバッファが確保されていなかったとき
+			vecImgBuffer.resize(buflen * NumCaptureImages);
+			bufcursor = vecImgBuffer.data();
+			bufend = bufcursor + vecImgBuffer.size();
+		}
+		if( bufend - bufcursor < buflen ) {
+			// 今から書こうとしている画像の分のバッファが足りないとき
+			// 今の画像サイズ*残り枚数分だけ追加で確保
+			int64_t curidx = bufcursor - vecImgBuffer.data();
+			int remainingImg = NumCaptureImages - i;
+			vecImgBuffer.resize(vecImgBuffer.size() + buflen * remainingImg);
+			bufcursor = vecImgBuffer.data() + curidx;
+			bufend = vecImgBuffer.data() + vecImgBuffer.size();
+		}
+		
+		pCap->CaptureRegion(
+			&windowRect,
+			bufcursor, bufend - bufcursor,
+			&sswidth, &ssheight);
+		bufcursor += buflen;
+		
+		vecImgSize.push_back(std::tuple<int, int>(sswidth, ssheight));
+		
+		TakenCount++;
+		
+	}
+	
+	// 書き出し
+	bufcursor = vecImgBuffer.data();
+	for( unsigned int i = 0; i < vecImgSize.size(); i++ ) {
+		char savename[128];
+		sprintf_s(savename, SavePathFormat.c_str(), i);
+		
+		const auto &size = vecImgSize[i];
+		sswidth = std::get<0>(size);
+		ssheight = std::get<1>(size);
+		
+		pWriter->Write(savename, sswidth, ssheight,
+			bufcursor, sswidth * ssheight * 3);
+		
+		bufcursor += sswidth * ssheight * 3;
+		
+	}
+	
+	vecImgSize.clear();
+	
+}
+
---
+++ AutoSS/ScreenShotBurst.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include <vector>
+#include <tuple>
+#include <thread>
+#include "ScreenShotBase.h"
+
+class ScreenShotBurst : public ScreenShotBase {
+public:
+	
+	ScreenShotBurst(
+		std::shared_ptr<CaptureBase> &pCap,
+		std::shared_ptr<ImageWriterBase> &pWriter,
+		const std::string &savePathFormat,
+		TRIMMING_MODE trimMode,
+		unsigned int numCaptureImages);
+	
+	virtual ~ScreenShotBurst();
+	
+	// 連写開始
+	virtual void Start();
+	
+	// 連写終了
+	virtual void Stop();
+	
+protected:
+	
+	// スクリーンショット撮影
+	void TakeSSFunc();
+	
+protected:
+	std::thread TakeThread;
+	std::vector<unsigned char> vecImgBuffer;
+	std::vector<std::tuple<int, int>> vecImgSize;
+	unsigned int NumCaptureImages;
+	bool StopFlag;
+};
+
