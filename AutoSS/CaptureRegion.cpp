adbdccbeeb22c7275fa8f7f81f0bd273209f30e0
lmdslyngl <lmdslyngl@users.noreply.github.com>
2017-07-01 17:36:30 +0900

    キャプチャ範囲を取得するCaptureRegionを作成した

---
+++ AutoSS/CaptureRegion.cpp
@@ -0,0 +1,105 @@
+﻿
+#include "CaptureRegion.h"
+
+/*
+ * 撮影範囲基底クラス
+*/
+
+CaptureRegionBase::CaptureRegionBase() {
+	// デスクトップの大きさを取得
+	HWND hDesktopWindow = GetDesktopWindow();
+	RECT windowRect;
+	GetWindowRect(hDesktopWindow, &windowRect);
+	DesktopWidth = windowRect.right - windowRect.left;
+	DesktopHeight = windowRect.bottom - windowRect.top;
+}
+
+// 画面外の領域をクランプする
+void CaptureRegionBase::ClampOutOfRegion(RECT *pInOutRegion) const {
+	if( pInOutRegion->left < 0 ) pInOutRegion->left = 0;
+	if( DesktopWidth < pInOutRegion->right ) pInOutRegion->right = DesktopWidth;
+	if( pInOutRegion->top < 0 ) pInOutRegion->top = 0;
+	if( DesktopHeight < pInOutRegion->bottom ) pInOutRegion->bottom = DesktopHeight;
+}
+
+
+/*
+ * 撮影範囲: 選択ウィンドウ
+*/
+void CaptureRegionSelectedWindow::GetCaptureRegionRect(RECT *pOutRegion) {
+	if( IsIncludeBorder() ) {
+		GetWindowRect(hCaptureWindow, pOutRegion);
+		
+	} else {
+		RECT clientRect;
+		GetClientRect(hCaptureWindow, &clientRect);
+		
+		POINT lt = { 0, 0 };
+		ClientToScreen(hCaptureWindow, &lt);
+		POINT rb = { clientRect.right, clientRect.bottom };
+		ClientToScreen(hCaptureWindow, &rb);
+		
+		pOutRegion->left = lt.x;
+		pOutRegion->top = lt.y;
+		pOutRegion->right = rb.x;
+		pOutRegion->bottom = rb.y;
+		
+	}
+	
+	ClampOutOfRegion(pOutRegion);
+	
+}
+
+
+/*
+ * 撮影範囲: アクティブウィンドウ
+*/
+void CaptureRegionActiveWindow::GetCaptureRegionRect(RECT *pOutRegion) {
+	HWND hCaptureWindow = GetForegroundWindow();
+	
+	if( IsIncludeBorder() ) {
+		GetWindowRect(hCaptureWindow, pOutRegion);
+		
+	} else {
+		RECT clientRect;
+		GetClientRect(hCaptureWindow, &clientRect);
+		
+		POINT lt = { 0, 0 };
+		ClientToScreen(hCaptureWindow, &lt);
+		POINT rb = { clientRect.right, clientRect.bottom };
+		ClientToScreen(hCaptureWindow, &rb);
+		
+		pOutRegion->left = lt.x;
+		pOutRegion->top = lt.y;
+		pOutRegion->right = rb.x;
+		pOutRegion->bottom = rb.y;
+		
+	}
+	
+	ClampOutOfRegion(pOutRegion);
+	
+}
+
+
+/*
+ * 撮影範囲: 選択範囲
+*/
+void CaptureRegionSelectedRegion::GetCaptureRegionRect(RECT *pOutRegion) {
+	pOutRegion->left = x;
+	pOutRegion->top = y;
+	pOutRegion->right = x + width;
+	pOutRegion->bottom = y + height;
+	ClampOutOfRegion(pOutRegion);
+}
+
+
+/*
+ * 撮影範囲: フルスクリーン
+*/
+void CaptureRegionFullscreen::GetCaptureRegionRect(RECT *pOutRegion) {
+	pOutRegion->left = 0;
+	pOutRegion->top = 0;
+	pOutRegion->right = DesktopWidth;
+	pOutRegion->bottom = DesktopHeight;
+}
+
---
+++ AutoSS/CaptureRegion.h
@@ -0,0 +1,109 @@
+﻿#pragma once
+
+#include <Windows.h>
+
+/*
+ * 撮影範囲基底クラス
+*/
+class CaptureRegionBase {
+public:
+	
+	CaptureRegionBase();
+	virtual ~CaptureRegionBase() = default;
+	virtual void GetCaptureRegionRect(RECT *pOutRegion) = 0;
+	
+protected:
+	
+	// 画面外の領域をクランプする
+	void ClampOutOfRegion(RECT *pInOutRegion) const;
+	
+protected:
+	int DesktopWidth, DesktopHeight;
+	
+};
+
+/*
+ * 撮影範囲: 選択ウィンドウ
+*/
+class CaptureRegionSelectedWindow : public CaptureRegionBase {
+public:
+	
+	CaptureRegionSelectedWindow(HWND hCaptureWindow, bool includeBorder)
+		: CaptureRegionBase()
+	{
+		this->hCaptureWindow = hCaptureWindow;
+		this->IsIncludeBorderFlag = includeBorder;
+	}
+	
+	virtual void GetCaptureRegionRect(RECT *pOutRegion);
+	
+	HWND GetCaptureWindow() const {
+		return hCaptureWindow;
+	}
+	
+	bool IsIncludeBorder() const {
+		return IsIncludeBorderFlag;
+	}
+	
+private:
+	HWND hCaptureWindow;
+	bool IsIncludeBorderFlag;
+};
+
+
+/*
+ * 撮影範囲: アクティブウィンドウ
+*/
+class CaptureRegionActiveWindow : public CaptureRegionBase {
+public:
+	CaptureRegionActiveWindow(bool includeBorder) 
+		: CaptureRegionBase()
+	{
+		this->IsIncludeBorderFlag = includeBorder;
+	}
+	
+	virtual void GetCaptureRegionRect(RECT *pOutRegion);
+	
+	bool IsIncludeBorder() {
+		return IsIncludeBorderFlag;
+	}
+	
+private:
+	bool IsIncludeBorderFlag;
+};
+
+
+/*
+ * 撮影範囲: 選択範囲
+*/
+class CaptureRegionSelectedRegion : public CaptureRegionBase {
+public:
+	CaptureRegionSelectedRegion(int x, int y, int width, int height)
+		: CaptureRegionBase()
+	{
+		this->x = x;
+		this->y = y;
+		this->width = width;
+		this->height = height;
+	}
+	
+	virtual void GetCaptureRegionRect(RECT *pOutRegion);
+	
+private:
+	int x, y, width, height;
+};
+
+
+/*
+ * 撮影範囲: フルスクリーン
+*/
+class CaptureRegionFullscreen : public CaptureRegionBase {
+public:
+	
+	CaptureRegionFullscreen() : CaptureRegionBase() { }
+	
+	virtual void GetCaptureRegionRect(RECT *pOutRegion);
+	
+};
+
+
