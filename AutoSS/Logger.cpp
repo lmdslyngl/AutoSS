8357b20d71cc4d079f681e5b1afe2d23094c8e8b
lmdslyngl <lmdslyngl@users.noreply.github.com>
2017-07-30 17:20:14 +0900

    ログを書き出すLoggerクラスを作成した

---
+++ AutoSS/Logger.cpp
@@ -0,0 +1,53 @@
+﻿
+#include "Logger.h"
+#include <time.h>
+#include <sstream>
+#include "Util.h"
+
+// ログ書き出しクラス
+Logger::Logger(const std::wstring &logfile) {
+	ofs.open(logfile, std::ios::app | std::ios::binary);
+}
+Logger::~Logger() {
+	ofs.close();
+}
+
+// メッセージ書き出し
+void Logger::Write(const wchar_t *log) {
+	std::lock_guard<std::mutex> mtxLock(mtx);
+	
+	std::wstringstream ss;
+	ss << L"[" << GetDateStr() << L"] " << log << L"\n";
+	std::string utf8line = ConvWStrToUtf8(ss.str());
+	ofs.write(utf8line.c_str(), utf8line.size());
+	
+}
+
+// メッセージ書き出し
+void Logger::Write(const std::wstring &log) {
+	Write(log.c_str());
+}
+
+// 現在時刻を表す文字列を取得
+std::wstring Logger::GetDateStr() {
+	time_t t = time(nullptr);
+	tm timedata;
+	localtime_s(&timedata, &t);
+	
+	std::wstringstream ss;
+	ss << timedata.tm_year + 1900 << L"/"
+		<< timedata.tm_mon + 1 << L"/"
+		<< timedata.tm_mday << L" "
+		<< timedata.tm_hour << L":"
+		<< timedata.tm_min << L":"
+		<< timedata.tm_sec;
+	
+	return ss.str();
+	
+}
+
+
+// グローバルLogger
+// logger実態
+std::unique_ptr<Logger> GlbLog::pLogger = nullptr;
+
---
+++ AutoSS/Logger.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+
+#include <string>
+#include <fstream>
+#include <mutex>
+#include <memory>
+#include <string.h>
+
+// ログ書き出しクラス
+class Logger {
+public:
+	
+	Logger(const std::wstring &logfile);
+	~Logger();
+	
+	// ログ書き出し
+	// 先頭に現在時刻が自動的に追加される
+	void Write(const wchar_t *log);
+	
+	// ログ書き出し
+	// 先頭に現在時刻が自動的に追加される
+	void Write(const std::wstring &log);
+	
+	//　フォーマットを指定してログ書き出し
+	template <class ... Args> void Write(const wchar_t *format, Args ... args) {
+		wchar_t buf[2048];
+		swprintf_s(buf, format, args...);
+		Write(buf);
+	}
+	
+private:
+	
+	// 現在時刻を表す文字列を取得
+	std::wstring GetDateStr();
+	
+private:
+	std::ofstream ofs;
+	std::mutex mtx;
+};
+
+
+// グローバルLogger
+class GlbLog {
+public:
+	
+	static void SetupGlobalLogger(const std::wstring &filename) {
+		pLogger = std::make_unique<Logger>(filename);
+	}
+	
+	static Logger &GetLogger() {
+		return *pLogger;
+	}
+	
+private:
+	static std::unique_ptr<Logger> pLogger;
+};
+
