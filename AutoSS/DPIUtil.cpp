8f5e2ba6dfc4901a235be3da4ccd7aff9c150580
lmdslyngl <lmdslyngl@users.noreply.github.com>
2017-08-11 14:35:15 +0900

    DPI関連のユーティリティDPIUtilを作成した

---
+++ AutoSS/DPIUtil.cpp
@@ -0,0 +1,30 @@
+﻿
+#include "DPIUtil.h"
+#include <Windows.h>
+
+/*
+ * DPI関連のユーティリティクラス
+*/
+
+DPIUtil::DPIUtil() {
+	SetProcessDPIAware();
+	
+	HDC hDC = GetDC(nullptr);
+	DpiX = GetDeviceCaps(hDC, LOGPIXELSX);
+	DpiY = GetDeviceCaps(hDC, LOGPIXELSY);
+	ReleaseDC(nullptr, hDC);
+	
+	ScalingX = DpiX / 96.0;
+	ScalingY = DpiY / 96.0;
+	
+}
+
+// GlobalDpiUtil
+std::unique_ptr<DPIUtil> pGlbDpiUtil = nullptr;
+
+// GlobalDpiUtilの初期化
+void SetupGlobalDpiUtil() {
+	pGlbDpiUtil = std::make_unique<DPIUtil>();
+}
+
+
---
+++ AutoSS/DPIUtil.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <memory>
+
+/*
+ * DPI関連のユーティリティクラス
+*/
+class DPIUtil {
+public:
+	
+	DPIUtil();
+	
+	int GetDpiX() const {
+		return DpiX;
+	}
+	int GetDpiY() const {
+		return DpiY;
+	}
+	
+	int GetScalingX() const {
+		return ScalingX;
+	}
+	int GetScalingY() const {
+		return ScalingY;
+	}
+	
+private:
+	int DpiX, DpiY;
+	double ScalingX, ScalingY;
+};
+
+// GlobalDpiUtil
+extern std::unique_ptr<DPIUtil> pGlbDpiUtil;
+
+// GlobalDpiUtilの初期化
+void SetupGlobalDpiUtil();
+
